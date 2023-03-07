b4b64ce6c1b798bb40246fe9f8169cd68939b364
dmkur <fgameua@gmail.com>
2023-03-07 14:38:38 +0000

    Update 7 files

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,44 @@
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
-      <div>
-        Home component
-      </div>
-  )
+    <div>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      <h1>Home component</h1>
+      
+    </div>
+  );
 };
 
-export {Home};
+export { Home };
